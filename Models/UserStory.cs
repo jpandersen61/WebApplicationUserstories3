e19281f480b289328f4b5f8fec03c83ca7b2aa13
Zealand-Jean <55512813+Zealand-Jean@users.noreply.github.com>
2022-09-05 06:39:11 +0200

    Add project files.

---
+++ Models/UserStory.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace WebApplicationUserstories.Models
+{
+    public class UserStory
+    {
+        private static int nextId=0;
+        public int Id { get;  set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        void NextID()
+        {
+            nextId++;
+            Id = nextId;
+        }
+
+        public UserStory()
+        {
+            Id = 0;
+            Title = "";
+            Description = "";
+        }
+
+        public UserStory(string title, string description)
+        {
+            NextID();
+            Title = title;
+            Description = description;
+        }
+
+
+    }
+}
